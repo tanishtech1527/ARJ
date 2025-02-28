745b08f40132bbe0cbb797d3c6f5dd81c3270ea0
tanishtech1527 <td412110@gmail.com>
2025-03-01 00:39:10 +0530

    chi1

---
+++ chi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdio.h>
+
+
+int main() {
+    int a,b;
+    printf("Enter first number :");
+    scanf("%d", &a);
+    printf("Enter second number :");
+    scanf("%d", &b);
+    
+    int sum = a+b;
+    printf("The sum is :%d\n",sum);
+
+    return 0;
+}
